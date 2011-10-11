dd5360a45aff12a599f4231c218163cd5a8be0ca
Shilo Ayalon <shiloayalon@gmail.com>
2011-10-11 13:06:03 +0200

    changed rspec tests to be more Rspec like

--- spec/globber_spec.rb
+++ spec/globber_spec.rb
@@ -14,19 +14,19 @@ describe Globber do
 		#****************************************
 		describe '#match' do
 			it "should return true when the given pattern exists in the input string" do
-				Globber.match('john', '*hn').should  == true
-				Globber.match('john', '*h*n').should == true
-				Globber.match('john like amy', '*like*').should == true
+				Globber.match('john', '*hn').should be_true
+				Globber.match('john', '*h*n').should be_true
+				Globber.match('john like amy', '*like*').should be_true
 			end
 
 			it "should return false when the given pattern doesn't exist in the input string" do
-				Globber.match('john', 'hn').should   == false
-				Globber.match('john', '*xy*').should == false
-				Globber.match('john hates sam', 'xyz').should == false
+				Globber.match('john', 'hn').should   be_false
+				Globber.match('john', '*xy*').should be_false
+				Globber.match('john hates sam', 'xyz').should be_false
 			end
 
       it "should properly account for module flags" do
-        Globber.match('john', '*HN', Globber::FNM_IGNORE_CASE).should  == true
+        Globber.match('john', '*HN', Globber::FNM_IGNORE_CASE).should  be_true
       end
 
       it "should raise TypeError when improper parameters are provided" do
@@ -37,15 +37,15 @@ describe Globber do
 
 		describe '#match_any_pattern' do
 			it "should return true when there's at least one matching pattern in the list" do
-				Globber.match_any_pattern('john', ['*hn', 'boe', 'pi*p']).should  == true
-				Globber.match_any_pattern('john', ['*hn', '*oh*', 'pi*p']).should == true
-				Globber.match_any_pattern('john likes amy', ['xz', '*like*', 'pi*p']).should == true
+				Globber.match_any_pattern('john', ['*hn', 'boe', 'pi*p']).should  be_true
+				Globber.match_any_pattern('john', ['*hn', '*oh*', 'pi*p']).should be_true
+				Globber.match_any_pattern('john likes amy', ['xz', '*like*', 'pi*p']).should be_true
 			end
 
 			it "should return false when there are no matching patterns in the list" do
-				Globber.match_any_pattern('john', ['hn', 'boe',  'pi*p']).should == false
-				Globber.match_any_pattern('john', ['hn', 'oh',  'pi*p']).should  == false
-				Globber.match_any_pattern('john hates sam', ['hn', 'oh',  'pi*p']).should  == false
+				Globber.match_any_pattern('john', ['hn', 'boe',  'pi*p']).should be_false
+				Globber.match_any_pattern('john', ['hn', 'oh',  'pi*p']).should  be_false
+				Globber.match_any_pattern('john hates sam', ['hn', 'oh',  'pi*p']).should  be_false
 			end
 
       it "should raise TypeError when improper parameters are provided" do
@@ -56,15 +56,15 @@ describe Globber do
 
 		describe '#match_any_string' do
 			it "returns true when there's at least one string in the list that matches the pattern" do
-				Globber.match_any_string('*hn', ['john', 'sam', 'jeeves']).should  == true
-				Globber.match_any_string('*am*', ['john', 'sam', 'jeeves']).should == true
-				Globber.match_any_string('*am*', ['john like amy', 'sam never met john', 'jeeves is a silly name']).should == true
+				Globber.match_any_string('*hn', ['john', 'sam', 'jeeves']).should  be_true
+				Globber.match_any_string('*am*', ['john', 'sam', 'jeeves']).should be_true
+				Globber.match_any_string('*am*', ['john like amy', 'sam never met john', 'jeeves is a silly name']).should be_true
 			end
 
 			it "returns false when there are not strings in the list that match the pattern" do
-				Globber.match_any_string('*hn', ['amy', 'sue',  'sally']).should == false
-				Globber.match_any_string('my', ['amy', 'sue',  'sally']).should  == false
-				Globber.match_any_string('sally misses sue', ['amy', 'sue',  'sally']).should == false
+				Globber.match_any_string('*hn', ['amy', 'sue',  'sally']).should be_false
+				Globber.match_any_string('my', ['amy', 'sue',  'sally']).should  be_false
+				Globber.match_any_string('sally misses sue', ['amy', 'sue',  'sally']).should be_false
 			end
 
       it "should raise TypeError when improper parameters are provided" do
@@ -83,8 +83,8 @@ describe Globber do
 
 		describe "#find_pattern" do
 			it "returns the pattern that matches the string, or nil if no matches were found" do
-				Globber.find_pattern('john', ['*hn', 'boe', 'pi*p', 'jo*']).should       == '*hn'
-				Globber.find_pattern('john likes amy', ['xz', '*likes*', 'pi*p']).should == '*likes*'
+				Globber.find_pattern('john', ['*hn', 'boe', 'pi*p', 'jo*']).should eq( '*hn' )
+				Globber.find_pattern('john likes amy', ['xz', '*likes*', 'pi*p']).should eq( '*likes*' )
 			end
 		end
 		describe "#find_all"
